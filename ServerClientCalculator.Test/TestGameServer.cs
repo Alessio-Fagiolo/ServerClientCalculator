0801947a3716282ff3cdf8a951c3f2e03c9051df
Alessio Fagiolo <lyno94@hotmail.it>
2019-03-16 13:33:33 +0100

    ServerCalculator

---
+++ ServerClientCalculator.Test/TestGameServer.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+namespace ServerClientCalculator.Test
+{
+    public class TestGameServer
+    {
+        private FakeTransport transport;
+        private GameServer server;
+
+        [SetUp]
+        public void SetupTests()
+        {
+            transport = new FakeTransport();
+            server = new GameServer(transport);
+        }
+
+        [Test]
+        public void checkAdd()
+        {
+            Packet add = new Packet(0, 1.0f,2.0f);
+            transport.ClientEnqueue(add, "test", 0);
+            server.SingleStep();
+
+            FakeData soluzione = transport.ClientDequeue();
+
+            Assert.That(soluzione.data[0], Is.EqualTo(4));
+
+        }
+
+        [Test]
+        public void checkAddResult()
+        {
+            Packet add = new Packet(0, 1.0f, 2.0f);
+            transport.ClientEnqueue(add, "test", 0);
+            server.SingleStep();
+
+            FakeData soluzione = transport.ClientDequeue();
+            float sol = BitConverter.ToSingle(soluzione.data, 1);
+            Assert.That(sol, Is.EqualTo(3));
+
+        }
+
+        [Test]
+        public void checkSub()
+        {
+            Packet sub = new Packet(1, 7.0f, 2.0f);
+            transport.ClientEnqueue(sub, "test", 0);
+            server.SingleStep();
+
+            FakeData soluzione = transport.ClientDequeue();
+
+            Assert.That(soluzione.data[0], Is.EqualTo(4));
+
+        }
+
+        [Test]
+        public void checkSubResult()
+        {
+            Packet sub = new Packet(1, 7.0f, 2.0f);
+            transport.ClientEnqueue(sub, "test", 0);
+            server.SingleStep();
+
+            FakeData soluzione = transport.ClientDequeue();
+            float sol = BitConverter.ToSingle(soluzione.data, 1);
+            Assert.That(sol, Is.EqualTo(5));
+
+        }
+
+
+        [Test]
+        public void checkMoltiplier()
+        {
+            Packet moltiplier = new Packet(2, 5.0f, 2.0f);
+            transport.ClientEnqueue(moltiplier, "test", 0);
+            server.SingleStep();
+
+            FakeData soluzione = transport.ClientDequeue();
+
+            Assert.That(soluzione.data[0], Is.EqualTo(4));
+
+        }
+
+        [Test]
+        public void checkMoltiplierResult()
+        {
+            Packet moltiplier = new Packet(2, 5.0f, 2.0f);
+            transport.ClientEnqueue(moltiplier, "test", 0);
+            server.SingleStep();
+
+            FakeData soluzione = transport.ClientDequeue();
+            float sol = BitConverter.ToSingle(soluzione.data, 1);
+            Assert.That(sol, Is.EqualTo(10));
+
+        }
+
+
+        [Test]
+        public void checkDivider()
+        {
+            Packet divider = new Packet(3, 8.0f, 2.0f);
+            transport.ClientEnqueue(divider, "test", 0);
+            server.SingleStep();
+
+            FakeData soluzione = transport.ClientDequeue();
+
+            Assert.That(soluzione.data[0], Is.EqualTo(4));
+
+        }
+
+        [Test]
+        public void checkDividerResult()
+        {
+            Packet moltiplier = new Packet(3, 8.0f, 2.0f);
+            transport.ClientEnqueue(moltiplier, "test", 0);
+            server.SingleStep();
+
+            FakeData soluzione = transport.ClientDequeue();
+            float sol = BitConverter.ToSingle(soluzione.data, 1);
+            Assert.That(sol, Is.EqualTo(4));
+
+        }
+    }
+}
---
+++ ServerClientCalculator/GameServer.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Net;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace ServerClientCalculator
+{
+    public class GameServer
+    {
+
+        private delegate void GameCommand(byte[] data, EndPoint sender);
+
+        private Dictionary<byte, GameCommand> commandsTable;
+
+        private Dictionary<EndPoint, GameClient> clientsTable;
+
+        private IGameTransport transport;
+
+        public uint NumClients
+        {
+            get
+            {
+                return (uint)clientsTable.Count;
+            }
+        }
+
+       
+        private void Add(byte[] data, EndPoint sender)
+        {
+            if(!clientsTable.ContainsKey(sender))
+            {
+                GameClient newClient = new GameClient(this, sender);
+                clientsTable[sender] = newClient;
+            }
+
+            float primoNum = BitConverter.ToSingle(data, 1);
+            float secondoNum = BitConverter.ToSingle(data, 5);
+
+            float sol = primoNum + secondoNum;
+            Packet solution = new Packet(4, sol);
+            clientsTable[sender].Enqueue(solution);
+            
+
+        }
+
+        private void Sub(byte[] data, EndPoint sender)
+        {
+            if (!clientsTable.ContainsKey(sender))
+            {
+                GameClient newClient = new GameClient(this, sender);
+                clientsTable[sender] = newClient;
+            }
+
+            float primoNum = BitConverter.ToSingle(data, 1);
+            float secondoNum = BitConverter.ToSingle(data, 5);
+
+            float sol = primoNum - secondoNum;
+            Packet solution = new Packet(4, sol);
+            clientsTable[sender].Enqueue(solution);
+
+
+        }
+
+        private void Moltiplier(byte[] data, EndPoint sender)
+        {
+            if (!clientsTable.ContainsKey(sender))
+            {
+                GameClient newClient = new GameClient(this, sender);
+                clientsTable[sender] = newClient;
+            }
+
+            float primoNum = BitConverter.ToSingle(data, 1);
+            float secondoNum = BitConverter.ToSingle(data, 5);
+
+            float sol = primoNum * secondoNum;
+            Packet solution = new Packet(4, sol);
+            clientsTable[sender].Enqueue(solution);
+
+
+        }
+
+        private void Divider(byte[] data, EndPoint sender)
+        {
+            if (!clientsTable.ContainsKey(sender))
+            {
+                GameClient newClient = new GameClient(this, sender);
+                clientsTable[sender] = newClient;
+            }
+
+            float primoNum = BitConverter.ToSingle(data, 1);
+            float secondoNum = BitConverter.ToSingle(data, 5);
+
+            float sol = primoNum / secondoNum;
+            Packet solution = new Packet(4, sol);
+            clientsTable[sender].Enqueue(solution);
+
+
+        }
+
+        public GameServer(IGameTransport gameTransport)
+        {
+            transport = gameTransport;
+            clientsTable = new Dictionary<EndPoint, GameClient>();
+            commandsTable = new Dictionary<byte, GameCommand>();
+            commandsTable[0] = Add;
+            commandsTable[1] = Sub;
+            commandsTable[2] = Moltiplier;
+            commandsTable[3] = Divider;
+        }
+
+
+        public void Run()
+        {
+
+            Console.WriteLine("server started");
+            while (true)
+            {
+                SingleStep();
+            }
+        }
+
+        private float currentNow;
+        public float Now
+        {
+            get
+            {
+                return currentNow;
+            }
+        }
+
+        public void SingleStep()
+        {
+            EndPoint sender = transport.CreateEndPoint();
+            byte[] data = transport.Recv(256, ref sender);
+            if (data != null)
+            {
+                byte gameCommand = data[0];
+                if (commandsTable.ContainsKey(gameCommand))
+                {
+                    commandsTable[gameCommand](data, sender);
+                }
+                else
+                {
+                    clientsTable[sender].Malus++;
+                }
+            }
+
+            foreach (GameClient client in clientsTable.Values)
+            {
+                client.Process();
+            }
+
+          
+
+        }
+
+
+        public bool Send(Packet packet, EndPoint endPoint)
+        {
+            return transport.Send(packet.GetData(), endPoint);
+        }
+
+        public void SendToAllClients(Packet packet)
+        {
+            foreach (GameClient client in clientsTable.Values)
+            {
+                client.Enqueue(packet);
+            }
+        }
+
+        public void SendToAllClientsExceptOne(Packet packet, GameClient except)
+        {
+            foreach (GameClient client in clientsTable.Values)
+            {
+                if (client != except)
+                    client.Enqueue(packet);
+            }
+        }
+
+
+        public GameClient GetClientFromEndPoint(EndPoint endpoint)
+        {
+            if (clientsTable.ContainsKey(endpoint))
+            {
+                return clientsTable[endpoint];
+            }
+            return null;
+        }
+
+    }
+}
